20abc74a0e327afce335a8a68664c70e8e75434a
vikasco987 <114792449+vikasco987@users.noreply.github.com>
2025-04-16 13:47:45 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const http = require('http')
+
+// Create a new socketio server
+const socketio = require('socket.io')
+const server = http.createServer(app)
+const io = socketio(server)
+
+io.on("connection", function (socket) {
+    socket.on("send-location", function (data) {
+        io.emit("receive-location", { id: socket.id, ...data })
+    })
+
+    socket.on("disconnect", function () {
+        io.emit("user-disconnect", { id: socket.id })
+    })
+})
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+server.listen(3000)
